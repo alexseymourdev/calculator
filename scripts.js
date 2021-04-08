248cf1f2d86c655524e7de9d9de4edce4bf77444
alexseymourdev <alexseymourdev@gmail.com>
2021-04-08 21:09:09 +0100

    adding calculator code to script

--- scripts.js
+++ scripts.js
@@ -48,17 +48,27 @@ function preview(event){
     console.log(dataType);
     if(dataType == 'operator'){
         if(number1){
-            strMessage = number1 + ' ' + currentItem;
+            operator = currentItem;
+            strMessage = number1 + ' ' + operator;
         } else {
             console.log('You cannot set an operator without a number being set');
         }
     } else {
-        if(number1){
-            number1 += currentItem;
+        if(operator){
+            if(number2){
+                number2 += currentItem;
+            } else {
+                number2 = currentItem;
+            }
+            strMessage = number1 + ' ' + operator + ' ' + number2;
         } else {
-            number1 = currentItem;
+            if(number1){
+                number1 += currentItem;
+            } else {
+                number1 = currentItem;
+            }
+            strMessage = number1;
         }
-        strMessage = number1;
     }
     objPreview.value = strMessage;
 }
@@ -69,4 +79,49 @@ function clear(event){
     operator = "";
     objPreview.value = "";
     objMaths.value = "";
+}
+
+//Adding a validation function for the numbers
+function isValidNumber(number){
+    //We are using a double negative as inNaN returns false on valid numbers
+    return !isNaN(number);
+}
+function calculator(number1,number2,operator){
+    //if number1 is not a number
+    if(!isValidNumber(number1)){
+        //end the function here and pass the message below.
+        return 'Argument 1 must be a number';
+    }
+    //if number 2 is not a number
+    if(!isValidNumber(number2)){
+        //end the function here and pass the message below.
+        return 'Argument 2 must be a number';
+    }
+    // if the operator does not equal + - * / %
+    if(operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%'){
+        //end the function here and pass the message below.
+        return 'Argument 3 must be an arithmatic operator';
+    }
+    //all fo the validation has passed so we need to do maths
+    var sum;
+    //based on the operator passed in argument 3 we will do a different sum
+    switch(operator){
+        case '+':
+            sum = parseFloat(number1) + parseFloat(number2);
+        break;
+        case '-':
+            sum = number1 - number2;
+        break;
+        case '*':
+            sum = number1 * number2;
+        break;
+        case '/':
+            sum = number1 / number2;
+        break;
+        case '%':
+            sum = number1 % number2;
+        break;
+    }
+    //return the value of the sum
+    return sum;
 }
